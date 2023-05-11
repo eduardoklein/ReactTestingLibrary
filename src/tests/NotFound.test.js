b2689bd15f7adae3b0211e6bdc10fd9b29340c09
Eduardo Klein <eduardo.klein@live.com>
2023-05-10 23:40:53 -0300

    test: teste do componente NotFound implementado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import NotFound from '../pages/NotFound';
+
+test('Um h2 é criado na tela com o texto Page requested not found', () => {
+  render(
+    <BrowserRouter>
+      <NotFound />
+    </BrowserRouter>,
+  );
+  const notFoundHeader = screen.getByText(/Page requested not found/i);
+  expect(notFoundHeader).toBeInTheDocument();
+});
+test('Um h2 é criado na tela com o texto Page requested not found', () => {
+  render(
+    <BrowserRouter>
+      <NotFound />
+    </BrowserRouter>,
+  );
+  const cryngPikachu = screen.getByRole('img');
+  expect(cryngPikachu).toHaveAttribute('src', expect.stringContaining('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif'));
+});
